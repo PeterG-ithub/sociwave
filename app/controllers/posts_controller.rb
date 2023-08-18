9591dfa8cff08f2dffdf15e389fe8bbccfcbd650
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-18 14:05:29 -0700

    Setup controller and view to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,21 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post 
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(posts_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :body)
   end
 end
