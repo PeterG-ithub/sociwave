a04cf226b6d201a1f545a87f119b0e76c18b8da1
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-18 13:37:24 -0700

    rename @post to @posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @post = Post.all
+    @posts = Post.all
+  end
+
+  def new
+    @post 
   end
 end
