5cd035ee68adb281f333db4389cde25b564deae1
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-19 14:43:13 -0700

    Add Friend Request button with FR create method

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,6 +3,11 @@ class FriendRequestsController < ApplicationController
   end
 
   def create
+    sender = User.find(params[:sender_id])
+    receiver = User.find(params[:receiver_id])
+    puts receiver
+    fr = current_user.friend_requests.build(sender: sender, receiver: receiver)
+    fr.save
   end
 
   def destroy
