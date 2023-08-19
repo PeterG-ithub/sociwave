7ee2f988de4bfb9a22f6108aa770e5339a1f1b5e
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-18 17:13:19 -0700

    Add way for user to comment on post
    Used comments_controller with methods #new
    and #create. Added comment view files similar
    post view files.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.build
+  end
+
+  def create
+    @post = Post.find params[:post_id]
+    @comment = current_user.comments.new(comments_params)
+    @comment.post = @post
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:body)
+  end
 end
