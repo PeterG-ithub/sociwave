f9ded3066cd3c8ca654f2b1870093d8203c27ae0
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-19 16:10:15 -0700

    Destroy friendship model and generate friend model

--- db/migrate/20230819031147_create_friendships.rb
+++
@@ -1,9 +0,0 @@
-class CreateFriendships < ActiveRecord::Migration[7.0]
-  def change
-    create_table :friendships do |t|
-      t.timestamps
-    end
-    add_reference :friendships, :user, foreign_key: true
-    add_reference :friendships, :friend, foreign_key: { to_table: :users }
-  end
-end
