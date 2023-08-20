e5b1a79662e2425adb2a36b14110899e2633353a
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-19 18:17:53 -0700

    Add friend#create method
    to accept friends in friend_request
    index.html.erb

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,6 +3,10 @@ class FriendsController < ApplicationController
   end
 
   def create
+    friend = User.find(params[:friend])
+    new_friend = current_user.friends.build(friend: friend)
+    new_friend.save
+    redirect_to friends_path
   end
 
   def destroy
