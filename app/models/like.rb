d9e143ae5c7c4cb92a21b32eb52d9deb83cc06ae
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-21 11:12:32 -0700

    Prevent User from liking things more than once

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :likable, polymorphic: true
   belongs_to :user
+
+  validates :user, uniqueness: { scope: :likable, message: 'Already liked this' }
 end
