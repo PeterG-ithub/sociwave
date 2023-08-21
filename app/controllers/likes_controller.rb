6b296b26448624a7cd16f15c36e5a4355b8afb72
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-21 10:23:36 -0700

    Add like functionality to posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    like = current_user.likes.new(likable: post)
+    like.save
+  end
+
+  private
+
+  def likes_params
+    params.required(:like).permit(:likable_id, :likable_type)
+  end
 end
