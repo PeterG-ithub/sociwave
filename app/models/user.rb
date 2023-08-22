5b2c65ad29b4b5356ce2734188ef91c5d7c7550b
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-22 10:15:38 -0700

    Add from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
   has_many :friend_requests, foreign_key: :receiver
   has_many :friends, ->(user) { unscope(:where).where('user_id = ? OR friend_id = ?', user.id, user.id) }
   has_many :likes
+
+  devise :omniauthable, omniauth_providers: %i[github]
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
