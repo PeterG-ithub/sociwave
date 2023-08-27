dac120c82b2a4e3a423f2937718c7665ea30d242
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-27 11:38:04 -0700

    Remove title column from post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :body, presence: true
-  validates :title, uniqueness: true
+  validates :body, presence: true
   belongs_to :user
   has_many :comments
   has_many :likes, as: :likable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_012533) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_183544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_012533) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.bigint "user_id"
     t.datetime "created_at", null: false
