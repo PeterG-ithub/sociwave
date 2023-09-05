8121b0f9236514b77d615bbeebbc65dbe52ecf44
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-09-04 18:07:56 -0700

    add js for like count

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="like"
 export default class extends Controller {
   static values = {
-    imagel: String, imageu: String
+    imagel: String, 
+    imageu: String,
+    count: Number
   }
-  
-  static targets = ["like"]
+  static targets = ["like", "counter"]
   connect() {
     console.log('like controller is here')
   }
@@ -16,14 +17,19 @@ export default class extends Controller {
       this.likeTarget.setAttribute('src', this.imageuValue)
       this.likeTarget.classList.remove('liked')
       this.likeTarget.classList.add('unliked')
+      this.subtractLike()
     } else {
       this.likeTarget.setAttribute('src', this.imagelValue)
       this.likeTarget.classList.add('liked')
       this.likeTarget.classList.remove('unliked')
+      this.addLike()
     }
   }
 
   subtractLike() {
-    
+    this.counterTarget.innerText = this.countValue - 1
+  }
+  addLike() {
+    this.counterTarget.innerText = this.countValue + 1
   }
 }
