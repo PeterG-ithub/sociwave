926e107a0b29e0c188af98cff136d09bd2d53613
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-09-04 19:03:37 -0700

    add destroy method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,13 @@ class LikesController < ApplicationController
     like = current_user.likes.new(likable: comment) if params[:comment_id]
     like.save
   end
+
+  def destroy
+    like = Like.find(params[:id])
+    if like.destroy
+      flash[:success] = 'like was successfully deleted.'
+    else
+      flash[:error] = 'Failed to delete the like.'
+    end
+  end
 end
