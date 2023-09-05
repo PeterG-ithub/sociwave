9a0cca9c2faf451dd52b2c55f9adc6c545a5ddc9
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-09-04 17:24:15 -0700

    add toggle functionality to like image

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -2,11 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="like"
 export default class extends Controller {
+  static values = {
+    imagel: String, imageu: String
+  }
+  
+  static targets = ["like"]
   connect() {
     console.log('like controller is here')
   }
 
-  add() {
-    console.log('liked')
+  toggle() {
+    if(this.likeTarget.classList.contains("liked")) {
+      this.likeTarget.setAttribute('src', this.imageuValue)
+      this.likeTarget.classList.remove('liked')
+      this.likeTarget.classList.add('unliked')
+    } else {
+      this.likeTarget.setAttribute('src', this.imagelValue)
+      this.likeTarget.classList.add('liked')
+      this.likeTarget.classList.remove('unliked')
+    }
+  }
+
+  subtractLike() {
+    
   }
 }
